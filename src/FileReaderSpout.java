32f2b4ea5c0d2669e8dd32b476bc78c34952c542
hja <github@futureofwebtechnology.com>
2015-10-02 15:41:44 +0200

    implement part 2

--- src/FileReaderSpout.java
+++ src/FileReaderSpout.java
@@ -1,9 +1,11 @@
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
@@ -13,76 +15,86 @@ import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 
 public class FileReaderSpout implements IRichSpout {
-  private SpoutOutputCollector _collector;
-  private TopologyContext context;
+
+    private SpoutOutputCollector _collector;
+    private TopologyContext context;
+
+    private BufferedReader br;
+
+    @Override
+    public void open(Map conf, TopologyContext context,
+                     SpoutOutputCollector collector) {
+
+        Objects.requireNonNull(conf);
+        Objects.requireNonNull(conf.get("inputFile"));
+
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(
+                    Paths.get(conf.get("inputFile").toString()).toFile().getAbsolutePath())));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        this.context = context;
+        this._collector = collector;
+    }
+
+    @Override
+    public void nextTuple(){
+
+        String line;
+        try {
+            if ((line = br.readLine()) != null) {
+                _collector.emit(Arrays.asList(new Object[]{line}));
+
+            }else try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+        declarer.declare(new Fields("word"));
+
+    }
+
+    @Override
+    public void close() {
+
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
-  @Override
-  public void open(Map conf, TopologyContext context,
-                   SpoutOutputCollector collector) {
+    @Override
+    public void activate() {
+    }
 
-     /*
-    ----------------------TODO-----------------------
-    Task: initialize the file reader
+    @Override
+    public void deactivate() {
+    }
 
+    @Override
+    public void ack(Object msgId) {
+    }
 
-    ------------------------------------------------- */
+    @Override
+    public void fail(Object msgId) {
+    }
 
-    this.context = context;
-    this._collector = collector;
-  }
-
-  @Override
-  public void nextTuple() {
-
-     /*
-    ----------------------TODO-----------------------
-    Task:
-    1. read the next line and emit a tuple for it
-    2. don't forget to sleep when the file is entirely read to prevent a busy-loop
-
-    ------------------------------------------------- */
-
-
-  }
-
-  @Override
-  public void declareOutputFields(OutputFieldsDeclarer declarer) {
-
-    declarer.declare(new Fields("word"));
-
-  }
-
-  @Override
-  public void close() {
-   /*
-    ----------------------TODO-----------------------
-    Task: close the file
-
-
-    ------------------------------------------------- */
-
-  }
-
-
-  @Override
-  public void activate() {
-  }
-
-  @Override
-  public void deactivate() {
-  }
-
-  @Override
-  public void ack(Object msgId) {
-  }
-
-  @Override
-  public void fail(Object msgId) {
-  }
-
-  @Override
-  public Map<String, Object> getComponentConfiguration() {
-    return null;
-  }
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
 }
