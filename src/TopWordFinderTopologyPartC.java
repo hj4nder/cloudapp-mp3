638db06a2f24aac6a0a3124005cda9f19e627242
hja <github@futureofwebtechnology.com>
2015-10-04 14:54:25 +0200

    mp3-3 use shuffleGrouping

--- src/TopWordFinderTopologyPartC.java
+++ src/TopWordFinderTopologyPartC.java
@@ -27,7 +27,7 @@ public class TopWordFinderTopologyPartC {
 
     builder.setSpout("spout", new FileReaderSpout(), 1);
     builder.setBolt("split", new SplitSentenceBolt(), 8).shuffleGrouping("spout");
-    builder.setBolt("normalize", new NormalizerBolt(), 8).fieldsGrouping("split", new Fields("word"));
+    builder.setBolt("normalize", new NormalizerBolt(), 8).shuffleGrouping("split");
     builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("normalize", new Fields("word"));
 
 
